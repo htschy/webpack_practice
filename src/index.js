cf10dbd274c4ff59a29a8e0f363394dab70f6cab
htschy <92494732+htschy@users.noreply.github.com>
2022-11-23 14:41:33 +0900

    Install css-loader & style-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import my from "./modules/my.js";
+import "./modules/my.css"; // css の読み込み
 
 console.log("webpack!");
 my();
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js", // ファイル名を main.jsで生成
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
